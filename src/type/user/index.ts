8eff04da68ff1f6ee8cec200d6192b97717f6c8a
DCAOP <1295665482@qq.com>
2023-06-13 17:04:17 +0800

    add interface Person

--- src/type/user/index.ts
+++ src/type/user/index.ts
@@ -1,5 +1,19 @@
+import internal from "stream";
+
 export interface Token {
   value: string;
   expire: number;
   _id?: string;
+}
+
+export interface Person{
+  username: string;
+  email: string;
+  name: string;
+  avatar?: string;
+  createdTime?: Date;
+  school?: string;
+  description?: string;
+  sex?: internal; //0 女 1 男
+  company?: string;
 }
