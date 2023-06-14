c0cf98deaf4ffe0c840dca7445c4ff4f5621cf14
DCAOP <1295665482@qq.com>
2023-06-14 08:37:10 +0800

    feat: add Login.Vue

--- src/router/component.ts
+++ src/router/component.ts
@@ -3,3 +3,7 @@ import { defineAsyncComponent } from "vue";
 export const asyncHome = defineAsyncComponent({
   loader: () => import("@pages/Home.vue"),
 });
+
+export const asyncLogin = defineAsyncComponent({
+  loader: () => import("@pages/login/Login.VUe"),
+});
--- src/router/config.ts
+++ src/router/config.ts
@@ -1,10 +1,15 @@
 import { RouteRecordRaw } from "vue-router";
-import { asyncHome } from "./component";
+import { asyncHome, asyncLogin } from "./component";
 
 export const routes: RouteRecordRaw[] = [
   {
     path: "/",
     name: "home",
     component: asyncHome,
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: asyncLogin,
   },
 ];
