7ef733187e2ef0076407b6f4b03b1eee5bb64c24
DCAOP <1295665482@qq.com>
2023-06-16 15:03:13 +0800

    feat:Login.vue

--- src/hooks/login/useLogin.ts
+++ src/hooks/login/useLogin.ts
@@ -1,11 +1,7 @@
 import request from "@/network/request";
 import { PersonRes } from "@/type/user";
-import { Ref, ref } from "vue";
 
 const useLogin = () => {
-  const data = ref({}) as Ref<PersonRes>;
-  const error = ref(undefined) as Ref<string | undefined>;
-  const fetching = ref(true);
 
   const login = async (userinfo: any) => {
     const { data, whenFinish, error } = request<PersonRes>({
